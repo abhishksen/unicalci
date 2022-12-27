f495d4021e4be4c812447a1c15412e14f38a90e4
Abhishek <codmonk21@gmail.com>
2022-12-27 15:05:30 +0530

    Add files via upload

---
+++ script age.js
@@ -0,0 +1,79 @@
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+function ageCalculate() {
+    let today = new Date();
+    let inputDate = new Date(document.getElementById("date-input").value);
+    let birthMonth, birthDate, birthYear;
+    let birthDetails = {
+        date: inputDate.getDate(),
+        month: inputDate.getMonth() + 1,
+        year: inputDate.getFullYear()
+    }
+    let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth() + 1;
+    let currentDate = today.getDate();
+
+    leapChecker(currentYear);
+
+    if (
+        birthDetails.year > currentYear ||
+        (
+            birthDetails.month > currentMonth &&
+            birthDetails.year == currentYear) ||
+        (birthDetails.date > currentDate &&
+            birthDetails.month == currentMonth &&
+            birthDetails.year == currentYear)
+    ) {
+        alert("Not Born Yet");
+        displayResult("-","-","-");
+        return;
+    }
+    birthYear= currentYear-birthDetails.year;
+    if(currentMonth>=birthDetails.month){
+        birthMonth=currentMonth-birthDetails.month;
+    }
+    else{
+        birthYear--;
+        birthMonth=12+currentMonth-birthDetails.month;
+    }
+    if(currentDate>=birthDetails.date){
+        birthDate=currentDate-birthDetails.date
+    }
+    else{
+        birthDate--;
+        let days =months[currentMonth-2];
+        birthDate=days+currentDate-birthDetails.date;
+        if(birthMonth<0){
+            birthMonth=11;
+            birthYear--;
+        }
+    }
+    displayResult(birthDate,birthMonth,birthYear);
+}
+
+function displayResult(bDate,bMonth,bYear){
+    document.getElementById("years").textContent=bYear;
+    document.getElementById("months").textContent=bMonth;
+    document.getElementById("days").textContent=bDate;
+
+}
+
+function leapChecker(year) {
+    if (year % 4 == 0 || (year % 100 == 0 && year || year % 400 == 0)) {
+        months[1] = 29;
+    }
+    else {
+        months[1] = 28;
+    }
+}
